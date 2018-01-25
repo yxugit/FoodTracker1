0a96b5ae5c22e97d263a763b1fd7889f6bbd1540
Jingwen Zhang <jingwenzhang@Jingwens-MacBook-Air.local>
2018-01-25 09:19:39 -0800

    setup outlets

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,5 +14,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 }
 
