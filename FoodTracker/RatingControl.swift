e2b2f26d9024a473fb3784335d16c64eb5651591
Jingwen Zhang <jingwenzhang@Jingwens-MacBook-Air.local>
2018-01-26 08:54:35 -0800

    added RatingControl file, empty now

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jingwen Zhang on 1/26/18.
+//  Copyright © 2018 yxu. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
